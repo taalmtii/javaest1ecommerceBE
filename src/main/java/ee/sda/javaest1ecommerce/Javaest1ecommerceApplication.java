d80a5eab1e47aa32112848dfd577b63007c5c7d1
Guilherme Quirino <guilhermezq@gmail.com>
2020-11-14 10:20:01 +0200

    First Commit

---
+++ src/main/java/ee/sda/javaest1ecommerce/Javaest1ecommerceApplication.java
@@ -0,0 +1,13 @@
+package ee.sda.javaest1ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaest1ecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javaest1ecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/sda/javaest1ecommerce/Javaest1ecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.sda.javaest1ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaest1ecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
