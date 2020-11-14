7461104b5c96036f781cbce45a74df1e2eb3cbe3
Guilherme Quirino <guilhermezq@gmail.com>
2020-11-14 12:29:04 +0200

    stateless session

--- src/main/java/ee/sda/javaest1ecommerce/config/SecurityConfig.java
+++ src/main/java/ee/sda/javaest1ecommerce/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,7 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/product/create").authenticated()
                 .anyRequest().permitAll().and().csrf().disable()
-                .httpBasic();
+                .httpBasic().and()
+        .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);;
     }
 
     @Bean
